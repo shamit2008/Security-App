5d0c06f3d1bd677b4c8543ec3378944cab2e2391
Shaurya Mittal <shauryamittal5@gmail.com>
2021-05-11 21:15:45 -0700

    Updates

---
+++ precache-manifest.df3cbdd0973d3ca66e491505c6c201a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba04d0a8a7d06e458252",
+    "url": "/Security-App/src/HomeScreen.js/static/js/runtime~main.d0b7c734.js"
+  },
+  {
+    "revision": "7a17a85c5f666bb500b0",
+    "url": "/Security-App/src/HomeScreen.js/static/js/main.ed03fe3b.chunk.js"
+  },
+  {
+    "revision": "7a0d83f265a53057edad",
+    "url": "/Security-App/src/HomeScreen.js/static/js/2.eb545ddf.chunk.js"
+  },
+  {
+    "revision": "7a17a85c5f666bb500b0",
+    "url": "/Security-App/src/HomeScreen.js/static/css/main.4c88d578.chunk.css"
+  },
+  {
+    "revision": "367b7e41ee9be360f2188dde1ea37307",
+    "url": "/Security-App/src/HomeScreen.js/index.html"
+  }
+];
