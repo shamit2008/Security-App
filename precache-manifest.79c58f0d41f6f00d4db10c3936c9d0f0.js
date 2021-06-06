cfd3aa1545e25c25df9b103bf15065311b1de4e8
Shaurya Mittal <shauryamittal5@gmail.com>
2021-06-06 14:30:08 -0700

    Updates

---
+++ precache-manifest.79c58f0d41f6f00d4db10c3936c9d0f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02c4edbfe7d1a5d0ed16",
+    "url": "/Security-App/static/css/main.0669898d.chunk.css"
+  },
+  {
+    "revision": "02c4edbfe7d1a5d0ed16",
+    "url": "/Security-App/static/js/main.364f0654.chunk.js"
+  },
+  {
+    "revision": "c5281ada5b69e0ec9854",
+    "url": "/Security-App/static/js/runtime~main.d24b0218.js"
+  },
+  {
+    "revision": "dea05f875931cbc72415",
+    "url": "/Security-App/static/js/2.32108dc0.chunk.js"
+  },
+  {
+    "revision": "ac3614f97f51076c8853fa2739d0acab",
+    "url": "/Security-App/index.html"
+  }
+];
